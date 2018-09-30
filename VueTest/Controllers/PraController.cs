dbd8f59b9dd9d00e0911fde8201619e0fc0e0117
kuan <shadow061103@gmail.com>
2018-10-01 00:23:58 +0800

    書範例練習

---
+++ VueTest/Controllers/PraController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueTest.Controllers
+{
+    public class PraController : Controller
+    {
+        // GET: Pra
+        public ActionResult Test1()
+        {
+            return View();
+        }
+        public ActionResult Test2()
+        {
+            return View();
+        }
+        public ActionResult Test3()
+        {//if else 元素複用
+            return View();
+        }
+        public ActionResult 購物車()
+        {
+            //全選功能 選某項計算總價
+            //商品分類
+            return View();
+        }
+    }
+}
