e66b29c3c8f0ab8d9e6bdec7b620515b843a973e
Kuan Fu <shadow061103@gmail.com>
2018-12-27 17:43:14 +0800

    render

---
+++ VueTest/Controllers/Default1Controller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueTest.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ShopCart()
+        {
+            return View();
+        }
+        public ActionResult emit()
+        {//傳遞資料給父組件
+            return View();
+        }
+        public ActionResult emit2()
+        {//input & model
+            return View();
+        }
+        public ActionResult emit3()
+        {
+            return View();
+        }
+        public ActionResult Bus中介()
+        {//房仲bus 跟房東跟房客關係
+            return View();
+        }
+        public ActionResult Slot()
+        {//在組件裡面用父組件的內容
+            return View();
+        }
+        public ActionResult 作用域插槽()
+        {//直接看範例 重要
+            return View();
+        }
+        public ActionResult 動態組件()
+        {
+            return View();
+        }
+        public ActionResult xtemplate()
+        {
+            return View();
+        }
+        public ActionResult 組件練習()
+        {//watch
+            return View();
+        }
+        public ActionResult 組件練習2()
+        {//tabs標籤
+            return View();
+        }
+        public ActionResult Directive()
+        {//自定義指令 bind inserted update componentUpdated unbind
+            return View();
+        }
+    }
+}
---
+++ VueTest/Controllers/RenderController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueTest.Controllers
+{
+    public class RenderController : Controller
+    {
+        // GET: Render
+        public ActionResult Render()
+        { //簡單範例
+            return View();
+        }
+        public ActionResult Render2()
+        { //實際範例 比對
+            return View();
+        }
+        public ActionResult 重複組件()
+        {//重複多個組件
+            return View();
+        }
+        public ActionResult 重複組件2()
+        {//重複多個組件 clone
+            return View();
+        }
+        public ActionResult If()
+        {//用js的if 取代v-if
+            return View();
+        }
+        public ActionResult For()
+        {// js for
+            return View();
+        }
+        public ActionResult If2()
+        {
+            return View();
+        }
+        public ActionResult Model()
+        {//render v-model寫法
+            return View();
+        }
+        public ActionResult 按鍵修飾()
+        { //聊天發送內容
+            return View();
+        }
+        public ActionResult 函數化組件()
+        {//也可以用:is掛載組件
+            //透過context傳遞上下
+            //this.level改成context.props.level
+            //this.$slots.default  改成context.children
+            //functional參數
+            return View();
+        }
+        public ActionResult RenderTest()
+        { //排序的table
+            return View();
+        }
+        public ActionResult RenderTest2()
+        {//留言列表 可以改成template寫法
+            return View();
+        }
+    }
+}
